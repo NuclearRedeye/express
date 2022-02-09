db0e56c0d1339dc0c6954a35b72c198ecd7e83e7
NuclearRedeye <github@mneah.com>
2022-02-09 15:34:26 +0000

    feat: implement users controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,6 @@
 import { User } from '../interfaces/user';
+
+import { randomUUID } from 'crypto';
 import { Router } from 'express';
 
 export const userController = Router();
@@ -6,26 +8,80 @@ export const userController = Router();
 const users: Map<string, User> = new Map();
 
 function isUser(user: any): boolean {
-  return (user.firstName && user.lastName && user.email);
+  return (user.firstName && user.lastName && user.email) ? true : false;
 }
 
 userController.get('/', (request, response) => {
-  response.sendStatus(501);
+  const payload = [];
+  for (const [key, value] of users.entries()) {
+    payload.push({uuid: key, ...value});
+  }
+  response.status(200).json(payload);
 });
 
 userController.post('/', (request, response) => {
-  response.sendStatus(501);
+  const payload = request.body;
+  if (isUser(payload) === false) {
+    response.sendStatus(400);
+    return;
+  }
+
+  const userId = randomUUID();
+  users.set(userId, payload);
+  response.status(201).json({uuid: userId, ...payload});
 });
 
-userController.get('/:userid', (request, response) => {
-  response.sendStatus(501);
+userController.get('/:uuid', (request, response) => {
+  const uuid = request.params.uuid;
+  if (users.has(uuid) === false) {
+    response.sendStatus(404);
+    return;
+  }
+
+  response.status(200).json({uuid, ...users.get(uuid)});
 });
 
-userController.put('/:userid', (request, response) => {
-  response.sendStatus(501);
+userController.put('/:uuid', (request, response) => {
+  const uuid = request.params.uuid;
+  if (users.has(uuid)) {
+    response.sendStatus(409);
+    return;
+  }
+
+  const payload = request.body;
+  if (isUser(payload) === false) {
+    response.sendStatus(400);
+    return;
+  }
+
+  users.set(uuid, payload);
+  response.status(201).json({uuid, ...payload});
 });
 
-userController.delete('/:userid', (request, response) => {
-  response.sendStatus(501);
+userController.patch('/:uuid', (request, response) => {
+  const uuid = request.params.uuid;
+  if (users.has(uuid) === false) {
+    response.sendStatus(404);
+    return;
+  }
+
+  const patched = {...users.get(uuid), ...request.body};
+  if (!isUser(patched)) {
+    response.sendStatus(400);
+    return;
+  }
+
+  users.set(uuid, patched);
+  response.status(200).json({uuid, ...patched});
 });
 
+userController.delete('/:uuid', (request, response) => {
+  const uuid = request.params.uuid;
+  if (users.has(uuid) === false) {
+    response.sendStatus(404);
+    return;
+  }
+
+  users.delete(uuid);
+  response.sendStatus(204);
+});
--- test/user.spec.ts
+++ test/user.spec.ts
@@ -1,63 +1,89 @@
 
+import { randomUUID } from 'crypto';
 import { app } from '../src/app';
 import { default as request } from 'supertest';
 
 describe('User Controller', () => {
 
+  const uuid = randomUUID();
+
   const testUser = {
-    firstName: 'Joe',
-    lastName: 'Bloggs',
-    email: 'joe.bloggs@example.com'
+    firstName: 'Unit',
+    lastName: 'Test',
+    email: 'unit.test@example.com'
   }
 
+  it('POST /users creates a new user', async () => {
+    const response = await request(app).post('/users').send(testUser);
+    expect(response.status).toEqual(201);
+    expect(response.type).toEqual(expect.stringContaining('json'));
+    expect(response.body).toEqual(expect.objectContaining(testUser));
+  });
+
+  it('POST /users fails with invalid payload', async () => {
+    const response = await request(app).post('/users').send({"firstName": "Jane"});
+    expect(response.status).toEqual(400);
+  });
+
+  it('PUT /users/:uuid creates a new user with a specific id', async () => {
+    const response = await request(app).put(`/users/${uuid}`).send(testUser);
+    expect(response.status).toEqual(201);
+    expect(response.type).toEqual(expect.stringContaining('json'));
+    expect(response.body).toEqual({uuid, ...testUser});
+  });
+
+  it('PUT /users/:uuid fails if the user already exists', async () => {
+    const response = await request(app).put(`/users/${uuid}`).send(testUser);
+    expect(response.status).toEqual(409);
+  });
+
+  it('PUT /users/:uuid fails with invalid payload', async () => {
+    const response = await request(app).put(`/users/${randomUUID()}`).send({firstName: "Jane"});
+    expect(response.status).toEqual(400);
+  });
+
   it('GET /users returns all users', async () => {
-    const res = await request(app).get('/users');
-    expect(res.status).toEqual(200);
-    expect(res.type).toEqual(expect.stringContaining('json'));
-    expect(res.body).toEqual({});
+    const response = await request(app).get('/users');
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual(expect.stringContaining('json'));
+    expect(response.body).toEqual(expect.arrayContaining([{uuid, ...testUser}]));
   });
   
-  it('GET /users/:userid returns an error if the specified user does not exist', async () => {
-    const res = await request(app).get('/users/1');
-    expect(res.status).toEqual(404);
+  it('GET /users/:uuid returns the specified user', async () => {
+    const response = await request(app).get(`/users/${uuid}`);
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual(expect.stringContaining('json'));
+    expect(response.body).toEqual({uuid, ...testUser});
   });
 
-  it('POST /users creates a new user', async () => {
-    const res = await request(app).post('/users').send({testUser});
-    expect(res.status).toEqual(201);
-    expect(res.type).toEqual(expect.stringContaining('json'));
-    expect(res.body).toEqual({id: '1', ...testUser});
+  it('GET /users/:uuid returns an error if the specified user does not exist', async () => {
+    const response = await request(app).get(`/users/${randomUUID()}`);
+    expect(response.status).toEqual(404);
   });
 
-  it('GET /users/:userid returns the specified user', async () => {
-    const res = await request(app).get('/users/1');
-    expect(res.status).toEqual(200);
-    expect(res.type).toEqual(expect.stringContaining('json'));
-    expect(res.body).toEqual({id: '1', ...testUser});
-  });
-
-  it('PUT /users/:userid creates a new user with a specific id', async () => {
-    const res = await request(app).put('/users/2').send({testUser});
-    expect(res.status).toEqual(201);
-    expect(res.type).toEqual(expect.stringContaining('json'));
-    expect(res.body).toEqual({id: '2', ...testUser});
-  });
-
-  it('PUT /users/:userid fails to create a user that already exists', async () => {
-    const res = await request(app).put('/users/2').send({testUser});
-    expect(res.status).toEqual(409);
-  });
-
-  it('PATCH /users/:userid updates a user', async () => {
+  it('PATCH /users/:uuid partially updates a user', async () => {
     const patchedUser = {...testUser, firstName: 'Jane'};
-    const res = await request(app).patch('/users/1').send({firstName: 'Jane'});
-    expect(res.status).toEqual(200);
-    expect(res.type).toEqual(expect.stringContaining('json'));
-    expect(res.body).toEqual({id: '1', ...patchedUser});
+    const response = await request(app).patch(`/users/${uuid}`).send({firstName: 'Jane'});
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual(expect.stringContaining('json'));
+    expect(response.body).toEqual({uuid, ...patchedUser});
   });
 
-  it('DELETE /users/:userid deletes a user', async () => {
-    const res = await request(app).delete('/users/2');
-    expect(res.status).toEqual(204);
+  it('PATCH /users/:uuid fully updates a user', async () => {
+    const patchedUser = {firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com'};
+    const response = await request(app).patch(`/users/${uuid}`).send(patchedUser);
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual(expect.stringContaining('json'));
+    expect(response.body).toEqual({uuid, ...patchedUser});
+  });
+
+  it('DELETE /users/:uuid deletes a user', async () => {
+    const response = await request(app).delete(`/users/${uuid}`);
+    expect(response.status).toEqual(204);
+  });
+
+  it('DELETE /users/:uuid fails if the user does not exist', async () => {
+    const response = await request(app).delete(`/users/${uuid}`);
+    expect(response.status).toEqual(404);
   });
 });
